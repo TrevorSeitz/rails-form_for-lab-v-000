0e1c9188f892d0be6f20aa2eab16898c8895cc9a
Trevor Seitz <trevinla@gmail.com>
2018-07-01 15:02:30 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,30 @@
 class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new
+    @student.title = params[:title]
+    @student.description = params[:room_number]
+    @student.save
+    redirect_to school_class_path(@student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(params.require(:student))
+    redirect_to school_class_path(@student)
+  end
 end
