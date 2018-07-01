c63af3646889899ae6f123be6efbd3d5545fa267
Trevor Seitz <trevinla@gmail.com>
2018-07-01 14:45:22 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :students
-  resources :school_classes
+  resources :students, only: [:index, :show, :new, :create, :edit]
+  resources :school_classes, only: [:index, :show, :new, :create, :edit]
+  patch 'students/:id', to: 'students#update'
+  patch 'school_classes/:id', to: 'school_classes#update'
 end
