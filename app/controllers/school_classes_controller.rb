3c71f80b09ea8f2420fb495ead30673842bb3be5
Trevor Seitz <trevinla@gmail.com>
2018-07-01 16:02:33 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -29,7 +29,7 @@ class SchoolClassesController < ApplicationController
 
   private
 
-  def school_class(*args)
+  def school_class_params(*args)
     params.require(:school_class).permit(*args)
   end
 end
