87660e7639131e1c13be1fb387b620377cc19352
Trevor Seitz <trevinla@gmail.com>
2018-07-01 14:58:58 +0000

    automatically backed up by learn

--- app/controllers/school_class_controller.rb
+++ app/controllers/school_class_controller.rb
@@ -1,2 +1,30 @@
 class SchoolClassController < ApplicationController
+  def index
+    @school_classes = SchoolClass.all
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.new
+    @school_class.title = params[:title]
+    @school_class.description = params[:room_number]
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
+  end
 end
